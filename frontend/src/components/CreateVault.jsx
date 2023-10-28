dd5bb68fdc4e84be188997a394da4fc8a2ed260a
PriyanKishoreMS <priyankishore2000@gmail.com>
2023-10-28 14:09:17 +0530

    createvault ui

--- frontend/src/components/CreateVault.jsx
+++ frontend/src/components/CreateVault.jsx
@@ -1,33 +1,48 @@
-import { Input } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 
 const CreateVault = () => {
 	return (
 		<>
-			<div className='p-20'>
-				<div className='mt-4 mb-4'>
-					<h1>Provide your inputs</h1>
-					<div className='flex flex-wrap w-full gap-4 md:flex-nowrap'>
-						<Input
-							type='text'
-							label='Fund address'
-							className='text-white rounded-md'
-							color='primary'
-							variant='flat'
-							radius='sm'
-						/>
+			<div className='p-20 my-20'>
+				<div className='bg-secondary-500 p-10 backdrop-blur-md bg-opacity-70 rounded-xl'>
+					<div className='mt-4 mb-4'>
+						<h1 className='text-3xl text-white '>Create Vault</h1>
+						<p className='mb-10 text-sm'>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</p>
+						<div className='flex flex-wrap w-full gap-4 md:flex-nowrap'>
+							<Input
+								type='text'
+								label='Fund address'
+								className='text-white rounded-md'
+								color='primary'
+								variant='flat'
+								radius='sm'
+							/>
+						</div>
 					</div>
-				</div>
-				<div className='mb-6'>
-					<div className='flex flex-wrap w-full gap-4 md:flex-nowrap'>
-						<Input
-							type='text'
-							label='Developer address'
-							className='text-white rounded-md'
-							color='primary'
-							variant='flat'
-							radius='sm'
-						/>
+					<div className='mb-6'>
+						<div className='flex flex-wrap w-full gap-4 md:flex-nowrap'>
+							<Input
+								type='text'
+								label='Developer address'
+								className='text-white rounded-md'
+								color='primary'
+								variant='flat'
+								radius='sm'
+							/>
+						</div>
 					</div>
+					<Button
+						variant='flat'
+						color='default'
+						radius='sm'
+						size='lg'
+						border='default'
+					>
+						Create Vault
+					</Button>
 				</div>
 			</div>
 		</>
--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
-import { Card, CardFooter, Image, Button, Input } from "@nextui-org/react";
+import { useState, useEffect } from "react";
+import {
+	Card,
+	CardFooter,
+	Image,
+	Button,
+	Input,
+	Link,
+} from "@nextui-org/react";
 import NavBar from "./Navbar";
-import { useAddress } from "@thirdweb-dev/react";
+import { useAddress, useContract } from "@thirdweb-dev/react";
 import { createVault } from "../utils/createVault";
 import { RiBankLine } from "react-icons/ri";
 import { PiVaultFill } from "react-icons/pi";
@@ -12,12 +19,6 @@ const cardData = [
 	{ title: "Card 2", description: "Description for Card 2" },
 	{ title: "Card 3", description: "Description for Card 3" },
 	{ title: "Card 4", description: "Description for Card 4" },
-	// { title: "Card 5", description: "Description for Card 5" },
-	// { title: "Card 6", description: "Description for Card 6" },
-	// { title: "Card 7", description: "Description for Card 7" },
-	// { title: "Card 8", description: "Description for Card 8" },
-	// { title: "Card 9", description: "Description for Card 9" },
-	// { title: "Card 10", description: "Description for Card 10" },
 ];
 
 const Dashboard = () => {
@@ -31,19 +32,28 @@ const Dashboard = () => {
 
 	const [createdVault, setCreatedVault] = useState(false);
 	const address = useAddress();
+	const { contract, isLoading } = useContract();
+
+	useEffect(() => {
+		if (address !== undefined && !isLoading) {
+			// getVault(address, contract);
+		}
+	}, [address, contract]);
 	console.log(address);
 	return (
 		<>
 			<NavBar />
 			<div>
 				<div className='flex items-center p-10 ml-40'>
-					<Button className='relative bg-opacity-70 backdrop-filter backdrop-blur-md bg-blue-500 p-10 rounded-md shadow-md m-5 cursor-pointer h-50 w-50 flex flex-col'>
-						<div className='flex items-center mb-4'>
-							<PiVaultFill className='text-4xl text-gray-100 mr-4' />
-							<h2 className='text-2xl font-bold text-white'>Create Vault</h2>
-						</div>
-						<p className='text-gray-300'>Create a vault to deposit</p>
-					</Button>
+					<Link href='/create'>
+						<Button className='relative bg-opacity-70 backdrop-filter backdrop-blur-md bg-blue-500 p-10 rounded-md shadow-md m-5 cursor-pointer h-50 w-50 flex flex-col'>
+							<div className='flex items-center mb-4'>
+								<PiVaultFill className='text-4xl text-gray-100 mr-4' />
+								<h2 className='text-2xl font-bold text-white'>Create Vault</h2>
+							</div>
+							<p className='text-gray-300'>Create a vault to deposit</p>
+						</Button>
+					</Link>
 					<Button className='relative bg-opacity-70 backdrop-filter backdrop-blur-md bg-blue-500 p-10 rounded-md shadow-md m-5 cursor-pointer h-50 w-50 flex flex-col'>
 						<div className='flex items-center mb-4'>
 							<RiBankLine className='text-4xl text-gray-100 mr-4' />
